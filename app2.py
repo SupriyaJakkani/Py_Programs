67461e435e1ef89acbb03b6d4917524c593d7510
SupriyaJakkani <jakkanisupriya34@gmail.com>
2025-04-23 11:52:12 +0530

    commit

---
+++ app2.py
@@ -0,0 +1,8 @@
+# Madlib Game
+color = input("Enter a Color: ")
+plural_noun = input("Enter Plural_Noun: ")
+celebrity = input("Enter a Celebrity: ")
+
+print("Roses are" +  color)
+print(plural_noun + "are Blue")
+print("I Love" +  celebrity)
